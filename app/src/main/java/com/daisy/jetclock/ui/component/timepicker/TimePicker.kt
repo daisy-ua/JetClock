de1033b447da127655efaaffa0e7e4dbbf62b394
supervisor <eightnum.81@gmail.com>
2023-06-23 15:17:28 +0300

    chore: update time picker ui

--- app/src/main/java/com/daisy/jetclock/ui/component/timepicker/TimePicker.kt
+++ app/src/main/java/com/daisy/jetclock/ui/component/timepicker/TimePicker.kt
@@ -1,15 +1,11 @@
 package com.daisy.jetclock.ui.component.timepicker
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
@@ -19,37 +15,60 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.daisy.jetclock.constants.TimeFormat
+import com.daisy.jetclock.ui.component.drawable.RoundedLine
 import com.daisy.jetclock.ui.theme.JetClockTheme
 
 @Composable
 fun TimePicker(
     timeFormat: TimeFormat = TimeFormat.Hour12Format,
 ) {
-    val timeFormatter = TimeFormatter.apply { setTimeFormat(timeFormat) }
+    val timeFormatter = TimeFormatter(timeFormat)
+    val meridiemOptions = listOf("", "", "AM", "PM", "", "")
 
     Row(
         horizontalArrangement = Arrangement.Center,
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier.fillMaxWidth(),
     ) {
-        Spacer(
-            modifier = Modifier
-                .padding(top = 8.dp, end = 16.dp)
-                .height(2.dp)
-                .width(50.dp)
-                .background(MaterialTheme.colors.onBackground)
-                .align(Alignment.CenterVertically)
+        RoundedLine(
+            colors = listOf(
+                MaterialTheme.colors.primary,
+                MaterialTheme.colors.secondaryVariant,
+                MaterialTheme.colors.primaryVariant,
+            ),
+            modifier = Modifier.padding(top = 8.dp, end = 16.dp)
         )
-        WheelPicker(items = timeFormatter.hours, firstIndex = 0, alignment = Alignment.CenterStart)
-        Text(":", modifier = Modifier.align(Alignment.CenterVertically), fontSize = 28.sp)
-        WheelPicker(items = timeFormatter.minutes, firstIndex = 0, alignment = Alignment.CenterEnd)
-        Spacer(
-            modifier = Modifier
-                .padding(top = 8.dp, start = 16.dp)
-                .height(2.dp)
-                .width(50.dp)
-                .background(MaterialTheme.colors.onBackground)
-                .align(Alignment.CenterVertically)
+        WheelPicker(
+            items = timeFormatter.hours,
+            initialIndex = 0,
+            alignment = Alignment.Center,
+        )
+        Text(
+            text = ":",
+            modifier = Modifier.align(Alignment.CenterVertically),
+            color = MaterialTheme.colors.primaryVariant,
+            fontSize = 28.sp,
+        )
+        WheelPicker(
+            items = timeFormatter.minutes,
+            initialIndex = 0,
+            alignment = Alignment.Center,
+        )
+        if (timeFormat == TimeFormat.Hour12Format) {
+            WheelPicker(
+                items = meridiemOptions,
+                initialIndex = 1,
+                alignment = Alignment.Center,
+                isInfinite = false,
+            )
+        }
+        RoundedLine(
+            colors = listOf(
+                MaterialTheme.colors.primaryVariant,
+                MaterialTheme.colors.secondaryVariant,
+                MaterialTheme.colors.primary,
+            ),
+            modifier = Modifier.padding(top = 8.dp, start = 16.dp)
         )
     }
 }
