8c33a740ed9e3f8d72767fc4ade526bea681b541
daisy-ua <eightnum.81@gmail.com>
2024-12-30 18:41:29 +0200

    fix: add empty item to AlarmList to prevent overlap with FloatingActionButton

--- app/src/main/java/com/daisy/jetclock/presentation/ui/component/alarm/AlarmList.kt
+++ app/src/main/java/com/daisy/jetclock/presentation/ui/component/alarm/AlarmList.kt
@@ -2,7 +2,11 @@ package com.daisy.jetclock.presentation.ui.component.alarm
 
 import androidx.compose.animation.expandVertically
 import androidx.compose.animation.shrinkVertically
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Card
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.icons.Icons
@@ -11,8 +15,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.daisy.jetclock.domain.model.Alarm
-import com.daisy.jetclock.presentation.ui.component.alarm.AlarmColumnContent
 import com.daisy.jetclock.presentation.ui.component.animation.listitemplacement.animatedItemsIndexed
 import com.daisy.jetclock.presentation.ui.component.animation.listitemplacement.updateAnimatedItemsState
 import com.daisy.jetclock.presentation.ui.component.animation.swipetoaction.SwipeActions
@@ -25,7 +29,7 @@ fun AlarmList(
     alarmList: List<Alarm>,
     onAlarmClick: (Long) -> Unit,
     onAlarmDelete: (Alarm) -> Unit,
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
 ) {
     val animatedList by updateAnimatedItemsState(newList = alarmList) { item -> item.id }
 
@@ -57,6 +61,17 @@ fun AlarmList(
                     onAlarmClick = onAlarmClick
                 )
             }
+        }
+
+        item {
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(64.dp),
+                backgroundColor = MaterialTheme.colors.background,
+                shape = RoundedCornerShape(0.dp),
+                elevation = 2.dp,
+            ) {}
         }
     }
 }
